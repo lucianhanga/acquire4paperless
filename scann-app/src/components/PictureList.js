385fef3691d2509a77f55ee0834a71b15fd109c0
Lucian Hanga <lucianhanga@gmx.net>
2025-03-23 13:39:57 +0100

    Refactor PictureList component: update CSS for picture item container and adjust aspect ratio for better layout

--- scann-app/src/components/PictureList.js
+++ scann-app/src/components/PictureList.js
@@ -5,7 +5,9 @@ function PictureList({ pictures }) {
   return (
     <div className="picture-list">
       {pictures.map((picture, index) => (
-        <img key={index} src={picture} alt={`Picture ${index + 1}`} className="picture-item" />
+        <div key={index} className="picture-item-container">
+          <img src={picture} alt={`Picture ${index + 1}`} className="picture-item" />
+        </div>
       ))}
     </div>
   );
