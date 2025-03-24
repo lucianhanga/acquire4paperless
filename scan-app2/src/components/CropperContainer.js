7511be3cd64867aee673a6e4f33758ce7b242dd8
Lucian Hanga <lucianhanga@gmx.net>
2025-03-24 16:14:53 +0100

    Update aspect ratio in CropperContainer to A4 dimensions

--- scan-app2/src/components/CropperContainer.js
+++ scan-app2/src/components/CropperContainer.js
@@ -2,14 +2,14 @@ import React from "react";
 import Cropper from "react-easy-crop";
 import "./CropperContainer.css";
 
-function CropperContainer({ image, crop, zoom, aspect, onCropChange, onZoomChange, onCropComplete }) {
+function CropperContainer({ image, crop, zoom, onCropChange, onZoomChange, onCropComplete }) {
   return (
     <div className="cropper-container">
       <Cropper
         image={image}
         crop={crop}
         zoom={zoom}
-        aspect={aspect}
+        aspect={210 / 297} // A4 aspect ratio
         onCropChange={onCropChange}
         onZoomChange={onZoomChange}
         onCropComplete={onCropComplete}
