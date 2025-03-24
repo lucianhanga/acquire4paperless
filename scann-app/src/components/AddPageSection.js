d908d186534300713ef1896ea229eb35db4e259b
Lucian Hanga <lucianhanga@gmx.net>
2025-03-24 15:05:21 +0100

    Remove unused components, styles, and test files to clean up the codebase

--- scann-app/src/components/AddPageSection.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AddPageButton from "./AddPageButton";
-
-function AddPageSection({ onAddPage }) {
-  return <AddPageButton onClick={onAddPage} />;
-}
-
-export default AddPageSection;
